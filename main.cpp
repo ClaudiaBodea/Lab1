c5ae4094cf7418d16723e9d95a4281606477487d
Claudia Bodea <bodea.claudia.maria@gmail.com>
2019-01-28 21:50:33 +0200

    Exercitiul 3

--- main.cpp
+++ main.cpp
@@ -4,12 +4,14 @@ using namespace std;
 
 int main()
 {
-    int v;
-    cout << "Introduceti varsta ";
-    cin >> v;
-    if(v >18)
-        cout << "Majora" << endl;
+    int a,b;
+    cout << "Introduceti valoarea 1: ";
+    cin >> a;
+    cout << "Introduceti valoarea 2: ";
+    cin >> b;
+    if(a <b)
+        cout << "Crescatoare" << endl;
     else
-        cout << "Minora" << endl;
+        cout << "Ne-crescatoare" << endl;
     return 0;
 }
